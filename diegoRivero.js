fdbb00cb8aa3cdbfe5e9496247c51e110eedf81d
dmrivero87 <dmrivero87@gmail.com>
2022-09-14 22:05:30 -0300

    desafio complementario operadorez avanzados
    Incorpore operador ternario

--- diegoRivero.js
+++ diegoRivero.js
@@ -1,4 +1,4 @@
-let usuarios_registrados = [
+/*let usuarios_registrados = [
     {nombre: "Juana" , apellido: "Perez" , doc_id: 13382450 , direccion: "Fernandez Crespo 2154"},
     {nombre: "Diego" , apellido: "Rivero" , doc_id: 45579872 , direccion: "Yatay 1428"},
     {nombre: "Ignacio" , apellido: "Caballero" , doc_id: 43214560 , direccion: "Enrique Brito 987"},
@@ -8,155 +8,91 @@ let usuarios_registrados = [
     {nombre: "Felipe" , apellido: "Peluffo" , doc_id: 54789638 , direccion: "Cañas 12"},
     
 ]
-/*
-function buscar_doc_id (usuario){
-    return (prompt ("Ingrese su documento de identidad"))
-}
-
-function nuevo_registro_usuario(){
-    let registro_usuario = [];{
-        let doc_id = prompt("Ingrese su documento de identidad");
-        let nombre = prompt("Ingrese su nombre");
-        let apellido = prompt("ingrese su apellido");
-        let direccion = prompt("Ingrerse su direccion");
-
-        
-        let nuevo_usuario = new Usuario(nombre , apellido , doc_id, direccion);
-
-        registro_usuario.push( nuevo_usuario );
-        console.log(registro_usuario);
-    }
-    
-}
 */
-const productos = [
-    { id: 1, name : "Camiseta Principal", precio: 3500, img : 'imagenes/camiseta1-3.png'},
-    { id: 2, name : "Camiseta Alternativa", precio: 3250, img : 'imagenes/camiseta2-3.png'},
-    { id: 3, name : "Pantalon Principal", precio : 2500, img : 'imagenes/short 1-1.png'},
-    { id: 4, name : "Pantalon Alternativa", precio : 2250 , img : 'imagenes/short 2-1.png'},
-    { id: 5, name : "Camiseta Niño", precio: 2500 , img : 'imagenes/camiseta n 1-1.png'},
-    { id: 6, name : "Camiseta Running", precio: 2000, img : 'imagenes/camiseta 3-1.png'},
-    { id: 7, name : "Campera", precio: 4500, img : 'imagenes/campera 1-1.png'},
-]
 
+const form_registro = document.getElementById("registrarse");
+const registro_nombre = document.getElementById("nombre");
+const registro_apellido = document.getElementById("apellido");
+const registro_doc_id = document.getElementById("doc_id");
+const registro_direccion= document.getElementById("domicilio");
+const registro_pass= document.getElementById("password");
+const alerta_registro = document.getElementById("alerta-registro")
 
-
-/*
-
-
+const form_login = document.getElementById("log_usuario");
+const login_usuario = document.getElementById("usuario");
+const login_pass = document.getElementById("contraseña");
+const id_error = document.getElementById("error")
 let registro_usuario = [];
-let registro_pedido = [];
+
 
 class Usuario{
-    constructor(nombre,apellido,doc_id,direccion){
+    constructor(nombre,apellido,doc_id,direccion,password){
 
         this.nombre = nombre;
         this.apellido = apellido;
         this.doc_id = doc_id;
         this.direccion = direccion;
+        this.password = password;
     }
 }
 
-class Pedido{
-    constructor (articulo, talle, precio, cantidad, total_pedido){
+form_registro.addEventListener("click", (e) => {
 
-      
-        this.articulo = articulo;
-        this.talle = talle;
-        this.precio = precio;
-        this.cantidad = cantidad;
-        this.total_pedido = total_pedido;
+   e.preventDefault();
+
+    if (registro_nombre.value != "" && registro_apellido.value != "" && registro_doc_id.value != "" && registro_direccion.value !="" && registro_pass.value != "" ){
+    let new_user = new Usuario(registro_nombre.value, registro_apellido.value, registro_doc_id.value , registro_direccion.value, registro_pass.value );
+        
+    registro_usuario.push(new_user);
+    //console.log(registro_usuario);  
+    alerta_registro.innerHTML = `<p> Bienvenido ${registro_nombre.value} , su usuario es ${doc_id.value}</p>`;
+    
+    }else{
+        alerta_registro.innerHTML = `<p> Error : Verifique que no quede ninguna opcion vacia`;
     }
+})
 
-}
+form_login.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-
-function nuevo_pedido(){
-     
-        for (let i= 1 ; i<=10; i++){ //Arranco el cliclo en 1 para poder enumerar las compras
-            let articulo = prompt("Ingrese el articulo a comprar en tu pedido numero " + i);
-            let talle = prompt("Ingrese el talle de " + articulo);
-            let precio = parseInt( prompt("Ingrese el precio de " + articulo));
-            let cantidad = parseInt( prompt("Ingrese cantidad del articulo"));
-            let total_pedido = (precio * cantidad)
-             
-            let nuevo_pedido = new Pedido(articulo, talle, precio, cantidad, total_pedido);
-        
-            registro_pedido.push( nuevo_pedido );
-            console.log(registro_pedido);
+    let usuario_reg = login_usuario.value;
+    let pass_reg = login_pass.value;
     
-            let siguiente= prompt ("Desea seguir comprando? Si o NO?")
-                if (siguiente == "SI" || siguiente == "si" || siguiente == "Si"){
-                    continue
-                }
-                else if(siguiente == "NO" || siguiente == "no" || siguiente == "No") {
-                    break
-                } else (siguiente = prompt("No has ingresado una opcion valida. Ingresa Si o NO "))
+    for (let user_id of registro_usuario){
+        if (usuario_reg == user_id.doc_id && pass_reg == user_id.password ){
+            id_error.innerHTML = `<p>Bienvenido ${registro_nombre.value}</p>`
+            const usuario_JSON = JSON.stringify(registro_nombre.value)
+            localStorage.setItem("usuario" , usuario_JSON);
+           
+        }else {
+            id_error.innerHTML = `<p>NO COINCIDE USUARIO Y CONTRASEÑA, INTENTE NUEVAMENTE</p>`
+            
         }
+    }
+})
 
-    
-}
+/*let nombre_usuario = document.getElementById("nombre_usuario");
+let user = localStorage.getItem("usuario");
+nombre_usuario.innerText = user;*/
 
-function registro (){
+const productos = [
+    { id: 1, name : "Camiseta Principal", precio: 3500, img : 'imagenes/camiseta1-3.png', cantidad: 1, talle : "" },
+    { id: 2, name : "Camiseta Alternativa", precio: 3250, img : 'imagenes/camiseta2-3.png', cantidad: 1, talle : ""},
+    { id: 3, name : "Pantalon Principal", precio : 2500, img : 'imagenes/short 1-1.png', cantidad: 1, talle : ""},
+    { id: 4, name : "Pantalon Alternativa", precio : 2250 , img : 'imagenes/short 2-1.png', cantidad: 1, talle : ""},
+    { id: 5, name : "Camiseta Niño", precio: 2500 , img : 'imagenes/camiseta n 1-1.png', cantidad: 1, talle : ""},
+    { id: 6, name : "Camiseta Running", precio: 2000, img : 'imagenes/camiseta 3-1.png', cantidad: 1, talle : ""},
+    { id: 7, name : "Campera", precio: 4500, img : 'imagenes/campera 1-1.png', cantidad: 1, talle : ""},
+    { id: 8, name : "Camiseta Blanca", precio: 3250, img : 'imagenes/camiseta 4-2.png', cantidad: 1, talle : ""},
+]
 
-    if (registro == "SI" || registro == "si" || registro == "Si"){
-
-        let resultado_some = usuarios_registrados.some(buscar_doc_id);
-        
-        
-        if (resultado_some == true){
-        
-        console.log("El usuario esta registrado")
-        
-        }
-        
-        else if (resultado_some == false){
-        
-        console.log("no esta registrado")
-        
-        }    
-        
-        nuevo_pedido()
-
-    }else if(registro == "NO" || registro == "no" || registro == "No") {
-        
-        nuevo_registro_usuario()
-        
-        nuevo_pedido()
-        
-        
-    }else (registro = prompt("No has ingresado una opcion valida. Ingresa Si o NO "))
-    
-}
-
-const total_precio_compra = registro_pedido.reduce((acumulador, objeto) => {
-    return acumulador + objeto.total_pedido; }, 0) ;
-
-
-
-console.log("Monto total del pedido es: " + total_precio_compra)
-/*alert("Gracias por comprar en la tienda del club mas popular. Que disfutes tus productos. Total compra : " + total_precio_compra )
-*/
-
-let btn_registro = document.getElementById("registrarse");
-
-btn_registro.addEventListener("click" , function (){
-    
-    let nombre = document.getElementById("nombre");
-    let doc_id = document.getElementById("doc_id");
-    let mensaje = document.getElementById("mensaje");
-
-    let parrafo = document.createElement("p");
-    parrafo.innerText = ( "Hola " + nombre.value + "Te has registrado exitosamente, tu Usiario es " + doc_id.value);
-    parrafo.style.color = "green";
-    parrafo.style.fontSize ="45px"
-    mensaje.append(parrafo);
-
-});
+let talles = ["S", "M", "L", "XL"]
 
 const contenedor_productos = document.querySelector(".contenedor-compra");
-const contenedor_pedidos = document.querySelector(".listado-pedido")
-const orden_pedido = [];
+let tabla = document.getElementById("tbody");
+let tabla_total = document.getElementById("tbody2");
+let orden_pedido = [];
+let total_a_pagar = [];
 
 function mostrar_productos(){
     productos.forEach(function (producto) {
@@ -170,19 +106,25 @@ function mostrar_productos(){
         const titulo_producto = document.createElement("h2");
         titulo_producto.textContent = producto.name;
         titulo_producto.classList.add("titulo-producto");
-
+        const talle_producto = document.createElement("select");
+        talle_producto.id = "seleccion-talle"
+        talle_producto.innerHTML = `<select onchange="cambio_talle()" name="t"><option value="S" >S</option><option value="M" >M</option><option value="L" >L</option><option value="XL">XL</option></select>`
+        
         const precio_producto = document.createElement("h3");
-        precio_producto.textContent = producto.precio
+        precio_producto.innerHTML = `<h3> $ ${producto.precio}`
 
         const btn_compra = document.createElement("button");
         btn_compra.textContent = "Agregar al pedido";
         btn_compra.classList.add("btn-comprar");
         btn_compra.addEventListener("click", function(){
-        agregar_carrito(producto.id);
+        agregar_carrito(producto.id, talle_producto);
+       
         })
-        
+       
+       
         div_productos.appendChild(imagen_producto);
         div_productos.appendChild(titulo_producto);
+        div_productos.appendChild(talle_producto);
         div_productos.appendChild(precio_producto);
         div_productos.appendChild(btn_compra);
 
@@ -191,50 +133,158 @@ function mostrar_productos(){
     });
 }
 
+function cambio_talle(){
+    let talle_seleccion = document.getElementById("seleccion-talle").value;
+    
+    
+    console.log(talle_seleccion);
+ 
+}
+document.addEventListener("change", cambio_talle)
+
+
+let ver_carrito = document.getElementById("mostrar_carrito");
+
+ver_carrito.addEventListener("click" , function(){
+
+    let carrito = document.getElementById("carrito");
+
+    carrito.style.display != "none" ? carrito.style.display = "none" : carrito.style.display = "flex";
+
+
+})
 
 document.addEventListener("DOMContentLoaded", function() {
 
     mostrar_productos();
+    ver_pedido();
+  
 }) 
 
-function agregar_carrito(id){
+
+
+function agregar_carrito(id, talle){
+
    const compra_productos = productos.find( (producto) => {
-    return producto.id === id
+   return producto.id === id
    });
+   compra_productos.talle = talle.value;
+
    orden_pedido.push(compra_productos);
-   console.log(orden_pedido);
-   ver_pedido(orden_pedido);
+   const carrito_JSON = JSON.stringify(orden_pedido) ;
+   localStorage.setItem("carrito" , carrito_JSON);
+
+
+   const fila_carrito = document.createElement("tr");
+   fila_carrito.innerHTML += `<td><img class="imagen-productos" src="${compra_productos.img}"></img></td>
+                              <td>${compra_productos.name}</td>
+                              <td>${compra_productos.cantidad}</td>
+                              <td>${compra_productos.talle}</td>
+                              <td> $ ${compra_productos.precio}</td>
+                              <td><button class="btn-borrar">Borrar</button></td>`;
+
+        
+        
+    tabla.append(fila_carrito);
+
+        let total_precio_compra = orden_pedido.reduce((acumulador, objeto) => {
+            return acumulador + objeto.precio; }, 0) ;
+
+        let total_JSON = JSON.stringify(total_precio_compra);
+        localStorage.setItem("a_pagar", total_JSON);
+        let a_pagar = JSON.parse(localStorage.getItem("a_pagar"));
+        
+        tabla_total.innerHTML="";
+
+        let fila_total = document.createElement("tr");
+        fila_total.innerHTML += `<td>TOTAL COMPRA :  $           ${a_pagar}</td>
+                                 <button id = "btn-fin"class="btn-comprar">Comprar</button>`;
+
+        
+        
+        tabla_total.append(fila_total);                 
+
+
+        const btn_borra_fila = document.querySelectorAll(".btn-borrar");
+        //console.log(btn_borra_fila.target);
+        for (let boton of btn_borra_fila){
+            boton.addEventListener("click", borrar_fila)
+            
+        }
+
+        let btn_final = document.getElementById("btn-fin")
+        btn_final.addEventListener("click", compra_final)
 }
 
-function ver_pedido(carrito){
 
-    contenedor_pedidos.innerHTML= "";
 
-    carrito.forEach(function (producto) {
+function ver_pedido(){
 
-        const div_productos = document.createElement("div");
-           
-        const titulo_producto = document.createElement("h2");
-        titulo_producto.textContent = producto.name;
-        titulo_producto.classList.add("titulo-producto");
+  
+    orden_pedido = JSON.parse(localStorage.getItem("carrito")) || [];
+    total_a_pagar = JSON.parse(localStorage.getItem("a_pagar") )|| [];
+    tabla.innerHTML="";
+    
+    orden_pedido.forEach((producto)=>{
+        const fila_carrito = document.createElement("tr");
+        fila_carrito.innerHTML += `<td><img class="imagen-productos" src="${producto.img}"></img></td>
+                               <td>${producto.name}</td>
+                               <td>${producto.cantidad}</td>
+                               <td>${producto.talle}</td>
+                               <td> $ ${producto.precio}</td>
+                               <td><button class="btn-borrar-fila">Borrar</button></td>`;
 
-        const precio_producto = document.createElement("h3");
-        precio_producto.textContent = producto.precio
-
-        const btn_borrar = document.createElement("button");
-        btn_borrar.textContent = "Borrar";
-        btn_borrar.classList.add("btn-comprar");
-        btn_borrar.className = "borrar";
-       
+    
+    
+        tabla.append(fila_carrito);      
         
-        div_productos.appendChild(titulo_producto);
-        div_productos.appendChild(precio_producto);
-        div_productos.appendChild(btn_borrar);
+        tabla_total.innerHTML="";
+        let filas_total = document.createElement("tr");
+        filas_total.innerHTML = `<td>TOTAL COMPRA :  $           ${total_a_pagar}</td>
+                                 <button id = "btn-fin"class="btn-comprar">Comprar</button>`;
 
-        contenedor_pedidos.appendChild(div_productos);
+        
+        
+        tabla_total.append(filas_total);          
+        let btn_fin = document.getElementById("btn-fin")
+        btn_fin.addEventListener("click", compra_final)
+        
+        const btn_borra_fila = document.querySelectorAll(".btn-borrar-fila");
+        //console.log(btn_borra_fila);
+        for (let boton of btn_borra_fila){
+            boton.addEventListener("click", borrar_fila)
+        }
+        
     })
 }
 
+function borrar_fila(e){
+    let abuelo = e.target.parentNode.parentNode;
+    let nombre = abuelo.cells[1].innerText;
+    let talle = abuelo.cells[3].innerText;
+    let carrito = JSON.parse(localStorage.getItem("carrito"));
+    let toDelete = carrito.find(x => x.name === nombre);
+    // carrito.remove(toDelete);
+    abuelo.remove();
+    
+        
+    }
 
+
+
+function compra_final(e){
+    total_a_pagar = JSON.parse(localStorage.getItem("a_pagar") )|| [];   
+    
+    tabla.innerHTML ="";
+    tabla_total.innerHTML ="";
+    let mensaje_final = document.createElement("tr");
+        mensaje_final.innerHTML = `<td>GRACIAS POR SU COMPRA :  $           ${total_a_pagar}</td>
+                                   <button onclick="location.reload()"class="btn-borrar">CONTINUAR</button>`;
+
+        
+        
+        tabla_total.append(mensaje_final);
+        localStorage.clear();
+}
 
 
